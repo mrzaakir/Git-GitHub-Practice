71efa2e58fb9ff1b58e02f8468a21485be1ac5f6
mrzaakir <mrzaakiraj@gmail.com>
2026-07-24 17:28:26 +0300

    waxan isbadal ku sameye files kan idil

--- Feature34Model.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo;
-// Feature 34 Model
-public class Feature34Model {
-    private String name = "Feature 34";
-}
--- Feature34Service.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-// Feature 34 Service
-public class Feature34Service {
-    public void execute() {
-        System.out.println("Feature 34 Service");
-    }
-}
