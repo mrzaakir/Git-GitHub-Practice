fa8f83cfd5062c1988918dd452744bd65d000ac0
mrzaakir <mrzaakiraj@gmail.com>
2026-08-08 20:31:35 +0300

    mdif: waxan kuso daray colors kale

--- calculator.js
+++ calculator.js
@@ -17,6 +17,10 @@
         --accent-2: #818cf8;
         --danger: #f87171;
         --shadow: 0 20px 50px rgba(0,0,0,0.35);
+        color-scheme: dark;
+        --bg: #1a91db;
+        --panel: #cb4d7ba5;
+        --panel-2: #44ca87;
       }
 
       * { box-sizing: border-box; }
